7a372a8f1e78946d50e4c8ace6b0b38c3a62baa2
Petr Skoda <petr.skoda@openlms.net>
2024-02-20 14:20:20 +1300

    OWORK-868 dev: prepare v2.4.0-RC1

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2023112500;
 $plugin->requires  = 2022112802.00; // 4.1.2 (Build: 20230313)
 $plugin->component = 'tool_certify'; // Full name of the plugin (used for diagnostics)
-$plugin->release   = 'v2.3+';
+$plugin->release   = 'v2.4';
 $plugin->supported = [401, 401];
 
 $plugin->dependencies = [
