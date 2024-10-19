c446eeda8ed2eb8c16014ac07250eab2f64e0769
Petr Skoda <petr.skoda@openlms.net>
2024-10-20 01:57:53 +1300

    OWORK-1207 dev: prepare Work 3.3.0-RC1

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2024091100;
 $plugin->requires  = 2023100902.00; // 4.3.2
 $plugin->component = 'tool_certify';
-$plugin->release   = 'v3.3.0dev';
+$plugin->release   = 'v3.3.0';
 $plugin->supported = [403, 403];
 
 $plugin->dependencies = [
